57e81041e32b569109b1c61854fecd9dbe39e394
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-24 17:39:44 +0100

    :zap: "Changed `index_range` method from static to instance method."

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -39,8 +39,7 @@ class Server:
         result = self.dataset()
         return result[start:end] if start < len(result) else []
 
-    @staticmethod
-    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
         """
         Return a tuple of size two containing a start index and an end index
 
