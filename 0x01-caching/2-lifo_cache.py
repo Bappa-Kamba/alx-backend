ce608e1fcf743583d5717959c21f7f21755f672f
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-27 23:06:39 +0100

    :tada: Implemented LIFO caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" LIFO Caching Module """
+from base_caching import BaseCaching
+from typing import Dict
+
+
+class LIFOCache(BaseCaching):
+    """ Defines a LIFO-cache that inherits from
+        `BaseCaching`.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.order_list = []
+
+    def put(self, key: str, item: str):
+        """ Adds an item into the cache.
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                oldest_key = self.order_list.pop(-1)
+                del self.cache_data[oldest_key]
+                print(f"DISCARD: {oldest_key}")
+            self.cache_data[key] = item
+            self.order_list.append(key)
+
+    def get(self, key: str) -> Dict:
+        """ Get an item by key
+        """
+        if not key:
+            return None
+        return self.cache_data.get(key, None)
